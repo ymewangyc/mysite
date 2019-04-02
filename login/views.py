310ba1fd4b12334f30fdca4735cef012c9580ee7
ymewangyc <wangyongchao_go@163.com>
2019-04-02 10:25:43 +0800

    注释初始化项目

---
+++ login/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+# Create your views here.
+from . import models
+
+def index(request):
+    pass
+    return render(request,"login/index.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        message = "所有字段都必须填写！"
+        if username and password:
+            username = username.strip()
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')  # TODO: redirect() request
+                else:
+                    message = "密码不正确"
+            except:
+                message = "用户名不存在"
+        return render(request,'login/login.html',{"message":message})
+    return render(request,"login/login.html")
+
+def register(request):
+    pass
+    return render(request,"login/register.html")
+
+def logout(request):
+    pass
+    return redirect("/index/")
