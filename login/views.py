a9ae4922355b7b8d0e9ef431e3a44da31dc5e9e3
ymewangyc <wang@example.com>
2019-04-02 22:28:53 +0800

    实战1-7

--- login/views.py
+++ login/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 
 # Create your views here.
 from . import models
+from . import forms
 
 def index(request):
     pass
@@ -10,21 +11,24 @@ def index(request):
 
 def login(request):
     if request.method == "POST":
-        username = request.POST.get('username',None)
-        password = request.POST.get('password',None)
-        message = "所有字段都必须填写！"
-        if username and password:
-            username = username.strip()
+        login_form = forms.UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
             try:
                 user = models.User.objects.get(name=username)
                 if user.password == password:
-                    return redirect('/index/')  # TODO: redirect() request
+                    return redirect('/index/')
                 else:
-                    message = "密码不正确"
+                    message = "密码不正确！"
             except:
-                message = "用户名不存在"
-        return render(request,'login/login.html',{"message":message})
-    return render(request,"login/login.html")
+                message = "用户不存在！"
+        return render(request, 'login/login.html', locals())
+
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
+
 
 def register(request):
     pass
@@ -33,3 +37,25 @@ def register(request):
 def logout(request):
     pass
     return redirect("/index/")
+
+def test(request):
+    if request.method == "POST":
+        login_form = forms.UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户不存在！"
+        return render(request, 'login/test.html', locals())
+
+    login_form = forms.UserForm()
+    return render(request, 'login/test.html', locals())
+
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^login/',views.login),
     url(r'^register/',views.register),
     url(r'^logout/',views.logout),
+    url(r'^test/',views.test),
 ]
